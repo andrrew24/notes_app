0e11c63421f16bf8161f891269432f8ae21b9140
Andrew <andrewwasfy24@gmail.com>
2023-06-29 02:46:52 +0300

    add => add note cubit and states

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,18 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+abstract class AddNoteState {}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailed extends AddNoteState {
+  final String error;
+
+  AddNoteFailed({
+    required this.error
+  });
+}
