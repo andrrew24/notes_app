59784113a7c00473124f250b255ab7103a0cb1e8
Andrew <andrewwasfy24@gmail.com>
2023-06-29 16:46:48 +0300

    add getNotes method to fetch all notes from Hive

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note.dart';
+
+import '../../constants/constant.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  getNotes() {
+    try {
+      var notesBox = Hive.box<Note>(hiveBoxName);
+      List<Note> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes: notes));
+    } on Exception catch (e) {
+      emit(NotesFail(errName: e.toString()));
+    }
+  }
 }
