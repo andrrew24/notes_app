ab857f80308351a52febd6be49f8c42bfac0b5f8
Andrew <andrewwasfy24@gmail.com>
2023-06-29 03:04:06 +0300

    register type adapter for note adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants/constant.dart';
+import 'package:notes_app/models/note.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(hiveBoxName);
+  Hive.registerAdapter(
+    NoteAdapter()
+  );
   runApp(const NotesApp());
 }
 
