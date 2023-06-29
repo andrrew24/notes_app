13efe0b0b9e6e5ee7c2630a8fc49c6485ca42568
Andrew <andrewwasfy24@gmail.com>
2023-06-29 03:18:37 +0300

    add logic to add_note_cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+
+import '../../constants/constant.dart';
+import '../../models/note.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(Note note) async {
+    try {
+      emit(AddNoteLoading());
+      var notesBox = Hive.box(hiveBoxName);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } on Exception catch (e) {
+      emit(AddNoteFailed(error: e.toString()));
+    }
+  }
 }
